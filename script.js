03997e5b90e2d4379094437a7a00620694aa9d92
Pavel <pavelsuspicin@gmail.com>
2022-03-03 13:45:55 +0300

    030322

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+let today = new Date()
+let cmas = new Date(today.getFullYear(), 11, 25)
+if (today.getMonth() === 11 && today.getDate() > 25) {
+    cmas.setFullYear(cmas.getFullYear() + 1)
+}
+let oneDay = 1000 * 60 * 60 * 24
+console.log(`${Math.ceil((cmas.getTime() - today.getTime()) / oneDay)}`)
